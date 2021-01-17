42ff961be0ad216dd449275251531bca68499c69
Cosmic Red <77282790+CosmicRed@users.noreply.github.com>
2021-01-17 21:40:09 +0530

    Changes

---
+++ Zr.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Zr {
+    public static void main(String[]args){
+        System.out.println("Write your name here: ");
+        Scanner zc = new Scanner(System.in);
+        String name = zc.next();
+        System.out.println("Hello " + name + ", Have a good day!");
+    }
+
+}
